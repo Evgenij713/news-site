136155d634db69bd7259acb92eaf1adb9b358006
Evgenij713 <109352500+Evgenij713@users.noreply.github.com>
2025-06-15 18:44:25 +0500

    Update all.php
    Изменил название переменной в цикле foreach

--- controllers/news/all.php
+++ controllers/news/all.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 $pageTitle = 'Главная страница';
 $articles = getALLArticles();
-foreach ($articles as $id_c => $article) {
-    $articles[$id_c]['title'] = htmlspecialchars(cropText($article['title']));
-    $articles[$id_c]['text_preview'] = htmlspecialchars(cropText($article['content']));
+foreach ($articles as $key => $article) {
+    $articles[$key]['title'] = htmlspecialchars(cropText($article['title']));
+    $articles[$key]['text_preview'] = htmlspecialchars(cropText($article['content']));
 }
 $pageContent = template('articles/v_all', ['articles' => $articles]);
